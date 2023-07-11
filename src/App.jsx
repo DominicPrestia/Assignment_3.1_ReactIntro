6148a93bb8213d11f77e4fbf29a24d0ff67867e7
Dominic <dominicprestia0@gmail.gmail.com>
2023-07-10 22:56:47 -0500

    Added image, centered content, add <hr> tags

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,29 @@ import './App.css'
 
 function App() {
 
-  return( 
+  return (
     <>
-      <h1>Dominic Prestia</h1>
-      <h2>Aspiring Software Engineer</h2>
 
-      <img src={profilePicture} alt="profileImage" />
+      <div className='centerAll'>
+        
+        <img src={profilePicture} alt="profileImage" />
+        <h1>Dominic Prestia</h1>
+        <h2>Aspiring Software Engineer</h2>
 
-      <h3>About me</h3>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam aliquid, veniam nam, maiores sequi labore, ipsa ab iure eaque pariatur porro doloremque consectetur incidunt! Corrupti quia dolorem nemo quam quo.
-      Architecto similique optio, dolores quae natus atque hic! Magnam laudantium quasi dicta rem voluptate illum saepe, dolorum nemo, explicabo, at ad dolores est itaque totam? Et vero qui natus incidunt!
-      Iste ducimus ab itaque, placeat incidunt dolore. Accusantium quia iusto quibusdam error odio aperiam fugit earum, sed minus cum aspernatur voluptatem ipsam, delectus odit cumque sit nemo deserunt. Voluptatum, ullam.</p>
-      
-      <h3>Contact me</h3>
 
-      <p>Email: dominicprestia0@gmail.com | Linkedin: www.linkedin.com/in/dominic-prestia</p>
+        <h3>About me</h3>
+        <hr />
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam aliquid, veniam nam, maiores sequi labore, ipsa ab iure eaque pariatur porro doloremque consectetur incidunt! Corrupti quia dolorem nemo quam quo.
+          Architecto similique optio, dolores quae natus atque hic! Magnam laudantium quasi dicta rem voluptate illum saepe, dolorum nemo, explicabo, at ad dolores est itaque totam? Et vero qui natus incidunt!
+          Iste ducimus ab itaque, placeat incidunt dolore. Accusantium quia iusto quibusdam error odio aperiam fugit earum, sed minus cum aspernatur voluptatem ipsam, delectus odit cumque sit nemo deserunt. Voluptatum, ullam.</p>
 
+        <h3>Contact me</h3>
+        <hr />
+        <p>Email: dominicprestia0@gmail.com | Linkedin: www.linkedin.com/in/dominic-prestia</p>
+        
+      </div>
 
     </>)
-  }
+}
 
 export default App;
